6a8cff044fb44f9bf07a0c1c3e1e76a732c45540
Gyeony95 <rlg1133@naver.com>
2025-06-03 23:12:09 +0900

    팬찬트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PrivacyPage from './pages/privacy/page'
 import PrivacyHowDayPage from './pages/privacy_how_day/page'
 import TemplatesPage from './pages/templates/page'
 import WithdrawPage from './pages/withdraw/page'
+import PrivacyFanChantPage from './pages/privacy_fan_chant/page'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/privacy" element={<PrivacyPage />} />
           <Route path="/privacy_how_day" element={<PrivacyHowDayPage />} />
+          <Route path="/privacy_fan_chant" element={<PrivacyFanChantPage />} />
           <Route path="/templates" element={<TemplatesPage />} />
           <Route path="/withdraw" element={<WithdrawPage />} />
         </Routes>
